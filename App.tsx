6ba32a7041fb92d7c5b5f7220a51d26546758a95
MelissaBG <melissabgoulart@gmail.com>
2022-07-08 21:58:24 -0300

    1 file modified

--- App.tsx
+++ App.tsx
@@ -1,14 +1,17 @@
 import * as React from 'react';
 import './style.css';
 import axios from 'axios';
-function buscarDados(usuario){
- const URL = `https://api.github.com/users/${usuario}`;
- const resultado = axios.get(url);
- console.log(resultado);
+function buscarDados(usuario) {
+  const URL = `https://api.github.com/users/${usuario}`;
+  const resultado = axios.get(URL);
+
+  return resultado;
 }
 
 export default function App() {
-  buscarDados('mathiasgheno');
+  buscarDados('mathiasgheno').then((resultado) => {
+    console.log(resultado);
+  });
   return (
     <div>
       <h1>Hello StackBlitz!</h1>
